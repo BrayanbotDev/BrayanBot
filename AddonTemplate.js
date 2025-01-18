d042f6306e974288703d27fc7c4948f01ac510e9
59L <60123007+59L@users.noreply.github.com>
2025-01-18 17:06:45 -0500

    Updated Addon Template
    Updated setlog to use new logging system & added developer information examples

--- AddonTemplate.js
+++ AddonTemplate.js
@@ -3,7 +3,8 @@ import { Addon } from "../../src/Modules/Structures/Handlers/Addons.js";
 import Utils from "../../src/Modules/Utils.js";
 import chalk from "chalk";
 
-const addon = new Addon("test", "v1.1.1"),
+// Construct the addon including its name and version.
+const addon = new Addon("AddonTemplate", "v1.1.1"),
   addonConfig = {
     config: {
       test: true,
@@ -28,18 +29,24 @@ const addon = new Addon("test", "v1.1.1"),
 /** @type {addonConfig} */
 const { config, lang, commands } = addon.customConfig(addonConfig);
 
-addon.setLog(
-  chalk.hex("#f52486").bold("[") +
-    chalk.hex("#e01e37").bold("BryanBot") +
-    chalk.hex("#f52486").bold("]"),
-  chalk.bold("TesT"),
-  "has been loaded!",
-  `Version: ${chalk.bold(`v1.1.1`)}`
-);
 
+// Declare the log message to be sent when the addon loads.
+// Refer to BryanBot Docs on how to format log messages for BryanBot Console Logging
+addon.setLog(`prefix{cyan{${addon.name}}} has been green{loaded}! Version: bold{${addon.version}}`);
+
+// Declare the developer of the addon for future credit and support issues
+// Declare your main alias and optionaly your Discord Invite, Document website and an o
+addon.setDeveloper("BryanBot") // Developer Name (required)
+  .setDiscord("https://bryanbot.dev/discord") // Your discord invite link or redirect (optional)
+  .setDocs("https://bryanbot.dev/docs") // Documentation reguarding this addon (optional)
+  .setAdditional("Thank you for using BryanBot. We are happy to assist you if you have any further questions. Please check out our discord!") // Extra information for the addon (optional) 
+
+
+// The main function to be executed when the addon is loaded
 addon.setExecute(async (manager) => {
   console.log("TesT addon executed!");
 
+  // Register a new command with the BryanBot CommandHandler
   new Command({
     commandData: commands.Test,
     commandConfig: {
